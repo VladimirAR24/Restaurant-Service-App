f365e696e502617e638b0107092f1aa0af30c610
VladimirAR24 <raekvladimir@gmail.com>
2024-11-01 15:38:14 +0300

    Инициализировал сервис Kitchen. Создал и настроил конфигурацию consume subscriber. Добавил докерфайл.

---
+++ Kitchen/Consumer/KitchenConsumer.cs
@@ -0,0 +1,24 @@
+﻿using Kitchen.Consumer.Messages;
+using MassTransit;
+
+namespace Kitchen.Consumer;
+
+public class KitchenConsumer : IConsumer<YourMessage>
+{
+    public async Task Consume(ConsumeContext<YourMessage> context)
+    {
+        var message = context.Message;
+
+        // Выполните алгоритмическую обработку данных
+        var result = ProcessMessage(message);
+
+        // Отправка результатов в другую очередь
+        await context.Publish(new YourResultMessage { Result = result });
+    }
+
+    private string ProcessMessage(YourMessage message)
+    {
+        // Ваш алгоритм обработки данных
+        return "Processed data";
+    }
+}
---
+++ Kitchen/Startup.cs
@@ -0,0 +1,68 @@
+﻿using Kitchen.Consumer;
+using MassTransit;
+
+namespace Kitchen;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // Метод для добавления сервисов в контейнер
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+
+        // Настройка MassTransit с RabbitMQ
+        services.AddMassTransit(x =>
+        {
+            // Регистрируем consumer для обработки сообщений
+            x.AddConsumer<KitchenConsumer>();
+
+            x.UsingRabbitMq((context, cfg) =>
+            {
+                cfg.Host("rabbitmq://localhost", h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+
+                // Определяем очередь, на которую будет подписан consumer
+                cfg.ReceiveEndpoint("kitchen_queue", e =>
+                {
+                    e.ConfigureConsumer<KitchenConsumer>(context);
+                });
+            });
+        });
+
+        // Включаем MassTransit Hosted Service
+        //services.AddMassTransitHostedService();
+    }
+
+    // Метод конфигурирует HTTP конвейер запросов
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}

6e6e1b32d832cc757c6e5d878d3140fbf33552de
da0it <nikulas.nikulin@yandex.ru>
2024-11-29 15:26:06 +0300

    Added DRF Endpoints

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from users.views import *
 
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+
+    path('api/', include('users.urls')),
 
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/docs/', SpectacularSwaggerView.as_view(url_name='schema')),
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,76 @@
-from django.shortcuts import render
+from rest_framework.exceptions import NotFound
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+from .models import User
+from users.serializers import UserSerializer
+from rest_framework.generics import ListAPIView, RetrieveAPIView, DestroyAPIView, RetrieveUpdateAPIView, CreateAPIView
 
-# Create your views here.
+#from django_filters.rest_framework import DjangoFilterBackend
+#from rest_framework.filters import SearchFilter
+from rest_framework import permissions
+
+#from rest_framework.response import Response
+#from rest_framework.views import APIView
+#from django.forms.models import model_to_dict
+
+class UserCreateAPIView(CreateAPIView):
+    queryset = User.objects.all()
+    model = User
+    serializer_class = UserSerializer
+
+    permission_classes = [
+        permissions.AllowAny
+    ]
+
+class UserListAPIView(ListAPIView):
+    queryset = User.objects.all()
+    model = User
+    serializer_class = UserSerializer
+
+    filterset_fields = ['id', 'username', 'is_active']
+    search_fields = ['username', 'email', 'first_name', 'last_name']
+
+    permission_classes = [
+        permissions.AllowAny
+    ]
+
+class UsernameRetrieveAPIView(RetrieveAPIView):
+    model = User
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def get_object(self):
+        User = get_user_model()
+        username = self.kwargs.get('username')
+
+        try:
+            return User.objects.get(username=username)
+        except User.DoesNotExist:
+            raise NotFound("User not found")
+
+class UserRetrieveAPIView(RetrieveAPIView):
+    queryset = User.objects.all()
+    model = User
+    serializer_class = UserSerializer
+
+    permission_classes = [
+        permissions.AllowAny
+    ]
+
+class RetrieveUpdateUserAPIView(RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    model = User
+    serializer_class = UserSerializer
+
+    permission_classes = [
+    permissions.AllowAny
+    ]
+
+class UserDestroyAPIView(DestroyAPIView):
+    queryset = User.objects.all()
+    model = User
+    serializer_class = UserSerializer
+
+    permission_classes = [
+        permissions.AllowAny
+    ]
